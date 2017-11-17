099c521d227aba36c87d140ef98f606de5e18e35
Alejandro Lazo <alazo8807@gmail.com>
2017-11-17 04:06:17 +0000

    add main pages to nodejs

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", function(req, res){
+   res.redirect("/landing"); 
+});
+
+app.get("/landing", function(req, res){
+   res.render("landing"); 
+});
+
+app.get("/homilies", function(req, res) {
+   res.render("homilies"); 
+});
+
+app.get("/bulletin", function(req, res) {
+   res.render("bulletin"); 
+});
+
+app.get("/calendar", function(req, res) {
+   res.render("calendar"); 
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
