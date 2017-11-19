96786f3766e07c0a40011dd8826665cc2ff5448a
Alejandro Lazo <alazo8807@gmail.com>
2017-11-19 15:42:50 +0000

    changes to navbar and calendar

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 var express = require("express"),
         app = express();
-        
+
+var isHomePage = true;        
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 
-app.use
+// app.use(function(req,res,next){
+//    res.locals.isHomePage = isHomePage;   
+// });
 
 app.get("/", function(req, res){
    res.redirect("/landing"); 
 });
 
 app.get("/landing", function(req, res){
-   res.render("landing"); 
+   res.render("landing", {isHomePage: true}); 
 });
 
 app.get("/homilies", function(req, res) {
-   res.render("homilies"); 
+   res.render("homilies", {isHomePage: false}); 
 });
 
 app.get("/bulletin", function(req, res) {
-   res.render("bulletin"); 
+   res.render("bulletin", {isHomePage: false}); 
 });
 
 app.get("/calendar", function(req, res) {
-   res.render("calendar"); 
+   res.render("calendar", {isHomePage: false}); 
 });
 
 
