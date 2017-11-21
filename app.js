5df2a4cec9c942f56ffe3365bab28ff9e365b539
Alejandro Lazo <alazo8807@gmail.com>
2017-11-21 00:54:38 +0000

    add flash message

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ var express = require("express"),
         app = express(),
  bodyParser = require("body-parser"),
  nodemailer = require('nodemailer'),
-smtpTransport = require('nodemailer-smtp-transport');
+smtpTransport = require('nodemailer-smtp-transport'),
+flash = require('connect-flash');
+
  
 var isHomePage = true;        
 
 app.set("view engine", "ejs");
 app.use( bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+app.use(flash());
 
-// app.use(function(req,res,next){
-//    res.locals.isHomePage = isHomePage;   
-// });
+
+app.use(require("express-session")({
+  secret: "This can be anything",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(function(req,res,next){
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
 app.get("/", function(req, res){
    res.redirect("/landing"); 
@@ -44,9 +56,11 @@ app.post("/", function(req,res){
    transporter.sendMail(mailOptions, function(error, info){
      if (error) {
        console.log(error);
+       req.flash("error", "Sorry, There was an error and your message could not be sent.")     //It has to be before redirect
        res.redirect("/landing");
      } else {
        console.log('Email sent: ' + info.response);
+       req.flash("success", "Thank you! You message has been sent.")     //It has to be before redirect
        res.redirect("/landing");
      }
    });
