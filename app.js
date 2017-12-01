501903cfbfdec0b5383d8e4cc84ac7044fa7d26a
Alejandro Lazo <alazo8807@gmail.com>
2017-12-01 01:53:13 +0000

    Add mlab credentials

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var User = require("./models/user");
 var pageId = "home";        
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/stjohns", {useMongoClient: true});
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds125556.mlab.com:25556/guadalupe", {useMongoClient: true});
+
+// mongoose.connect("mongodb://localhost/stjohns", {useMongoClient: true});
 
 app.set("view engine", "ejs");
 app.use( bodyParser.urlencoded({extended: true}));
